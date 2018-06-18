bee13840bd67940f2bda28d8f0257cdfbd815b23
Eric Johnson <ejohn156@uncc.edu>
2018-06-18 13:21:41 -0400

    top rated search based on city/state

--- javascript/script.js
+++
@@ -1,5 +0,0 @@
-//will create API queries for zomato and ticketmaster
-//this will help us determine what we need in our forms
-
-//Zomato API key: b8fefdb1eb1eef0859aad5778cee33ad
-//ticketMaster API key: cdS8dgGbDGzl3TTP71wEQpLkCA8G95Ig
