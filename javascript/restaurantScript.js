f283d95d54daa6f8244ef451dd19f4b6fb1a8d16
Victoria Paul <victoriapaul@wifi_guest-10-38-99-175.uncc.edu>
2018-06-21 10:01:36 -0400

    zomato api key swap

--- javascript/restaurantScript.js
+++ javascript/restaurantScript.js
@@ -2,7 +2,7 @@
 //this will help us determine what we need in our forms
 
 //Zomato API key: 8e3c68c31ec53ec6cbd05ad1cd8d8174
-//zomato api key2: 8e3c68c31ec53ec6cbd05ad1cd8d8174
+//zomato api key2: b8fefdb1eb1eef0859aad5778cee33ad
 
 //ticketMaster API key: cdS8dgGbDGzl3TTP71wEQpLkCA8G95Ig
 //pulled origin master
@@ -13,7 +13,7 @@ var success = false
 var city = "Charlotte" //default on page load city
 var state = "NC" //default on page load state
 var zomatoSearch = city + ", " + state
-var zomatoUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + zomatoSearch + "&count=1$apikey=8e3c68c31ec53ec6cbd05ad1cd8d8174"
+var zomatoUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + zomatoSearch + "&count=1$apikey=b8fefdb1eb1eef0859aad5778cee33ad"
 var cityID = ""
 var count = ""
 var establishment_type = ""
@@ -47,7 +47,7 @@ function search(x) {
         async: true,
         beforeSend: function (xhr) {
             xhr.setRequestHeader('user-key',
-                '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                'b8fefdb1eb1eef0859aad5778cee33ad');
         },
     }).then(function (response) {
         //console is cleared to keep it from filling up to much
@@ -71,7 +71,7 @@ function search(x) {
             state = $("#inputState").val().trim()//changes prev state to submitted city
             displayCity()//displays new city info on page
             zomatoSearch = city + ", " + state//changes zomatoSearch to new city/state
-            zomatoUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + zomatoSearch + "&count=1$apikey=8e3c68c31ec53ec6cbd05ad1cd8d8174"
+            zomatoUrl = "https://developers.zomato.com/api/v2.1/locations?query=" + zomatoSearch + "&count=1$apikey=b8fefdb1eb1eef0859aad5778cee33ad"
             //ajax function run
             $.ajax({
                 url: zomatoUrl,//pulls updated zomatoUrl
@@ -79,7 +79,7 @@ function search(x) {
                 async: true,
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('user-key',
-                        '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                        'b8fefdb1eb1eef0859aad5778cee33ad');
                 },
             }).then(function (response) {
 
@@ -98,7 +98,7 @@ function search(x) {
                     async: true,
                     beforeSend: function (xhr) {
                         xhr.setRequestHeader('user-key',
-                            '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                            'b8fefdb1eb1eef0859aad5778cee33ad');
                     },
                 }).then(function (response) {
                     console.clear()//clears console
@@ -126,7 +126,7 @@ function search(x) {
             async: true,
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('user-key',
-                    '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                    'b8fefdb1eb1eef0859aad5778cee33ad');
             },
         }).then(function (response) {
             //console.clear()//clears console
@@ -153,7 +153,7 @@ function search(x) {
             async: true,
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('user-key',
-                    '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                    'b8fefdb1eb1eef0859aad5778cee33ad');
             },
         }).then(function (response) {
             //console.clear()//clears console
@@ -180,7 +180,7 @@ function search(x) {
             async: true,
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('user-key',
-                    '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                    'b8fefdb1eb1eef0859aad5778cee33ad');
             },
         }).then(function (response) {
             //console.clear()//clears console
@@ -299,7 +299,7 @@ function showPosition(position) {
         async: true,
         beforeSend: function (xhr) {
             xhr.setRequestHeader('user-key',
-                '8e3c68c31ec53ec6cbd05ad1cd8d8174');
+                'b8fefdb1eb1eef0859aad5778cee33ad');
         },
     }).then(function (response) {
         console.log("working2")
